5b58ff5b5778240e40ede8a7c1ba9c677e400813
Giordano.dev <giordanocastilloalmanzar@gmail.com>
2026-09-15 11:48:52 -0400

    Exercise 1.4 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 const App = () => {
   const course = 'Half Stack application development'
-  
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
+  const parts =  [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
 
-  const part2 = {
-    name:  'Using props to pass data',
-    exercises: 7
-  }
+    {
+      name:  'Using props to pass data',
+      exercises: 7
+    },
   
-  const part3 ={
-    name:'State of a component',
-    exercises: 14
-  } 
+    {
+      name:'State of a component',
+      exercises: 14
+    } 
+  ]
+  
 
   return (
     <div>
       <Header course= {course}/>
-      <Content
-        part1 = {part1}
-        part2 = {part2}
-        part3 = {part3}
-      />
-      <Total total= {part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts= {parts}/>
+      <Total parts= {parts}/>
     </div>
   )
 }
@@ -44,19 +42,21 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1}/>
-      <Part part={props.part2}/>
-      <Part part={props.part3}/>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
     </div>
   )
 }
 
 const Total = (props) => {
+  const totalExercises = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+  console.log(totalExercises) 
   return(
     <div>
-      <h4>
-        And the total is {props.total}
-      </h4>
+      <p>
+        Total exercises: {totalExercises}
+      </p>
     </div>
   )
 }
@@ -65,9 +65,8 @@ const Part = (props) => {
   return (
     <div>
       <p>
-        {props.part.name} exercises:{props.part.exercises}
-        <br />
-
+        {props.part.name} {props.part.exercises}
+        
       </p>
     </div>
   )
